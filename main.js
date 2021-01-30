fcda81399226c270b223118edf47a0dcd66d9a0a
Jakub Gawron <kubagawron@spoko.pl>
2021-01-30 17:40:52 +0100

    init commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const message = new Discord.Message;
+const { prefix, token } = require('./config');
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(token);
+client.on('message', message => {
+    console.log(message.content);
+    if (message.content === `${prefix}ping`) {
+        message.channel.send('Pong.');
+    } else if (message.content === `${prefix}beep`) {
+        message.channel.send('Boop.');
+    }
+    else if (message.content === `${prefix}server`) {
+        message.channel.send(`This server's name is: ${message.guild.name}\nTotal members: ${message.guild.memberCount}\n Created server in ${message.guild.createdAt}\n and country of server: ${message.guild.region}`);
+    }
+});
